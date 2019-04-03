762dbfbf6a23423c53239c3e7519fa2d93202840
CodingOni <37724661+CodingOni@users.noreply.github.com>
2019-04-03 16:28:22 -0600

    Styling changes

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,9 @@
 if (screen.width <= 699) {
 window.location = "https://codingoni.github.io/portfolioWebsite/";
 }
+
+$(window).scroll(function() {
+  if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      alert("Click my profile picture to learn more!");
+  }
+});
