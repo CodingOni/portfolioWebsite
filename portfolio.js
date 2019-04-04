9a784651d6cfc751522ee97dc2982cdf30446f89
CodingOni <37724661+CodingOni@users.noreply.github.com>
2019-04-03 21:52:42 -0600

    Optimization

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,3 @@
 if (screen.width <= 699) {
 window.location = "https://codingoni.github.io/portfolioWebsite/";
 }
-
-$(window).scroll(function() {
-  if($(window).scrollTop() + $(window).height() == $(document).height()) {
-      alert("Click my profile picture to learn more!");
-  }
-});
